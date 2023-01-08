1b5ee283ccff7b65bc3438e8e44d308e0dc90299
Gonzalo <garciacalvog@hotmail.com>
2023-01-08 18:20:33 -0300

    PORT

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
 const config = {
   MONGO_DB:'mongodb+srv://garciacalvog:yJrrTE4mcwui4Ed@cluster0.k3ncstn.mongodb.net/test',
   MODE: "fork",
-  PORT: 8080
+  PORT: process.env.PORT || 8080
 }
+
 
 module.exports = config
