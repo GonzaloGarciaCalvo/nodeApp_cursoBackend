b89065b07a6cbb56ebd4abeb74ed594c5d7a167f
Gonzalo <garciacalvog@hotmail.com>
2023-01-08 18:34:01 -0300

    fix nombre file

---
+++ db/mongo.js
@@ -0,0 +1,10 @@
+/* const moongose = require ('mongoose');
+
+
+async function connection() {
+console.log("conexion en mognojs")
+    const URL = `mongodb+srv://garciacalvog:yJrrTE4mcwui4Ed@cluster0.qju9tzm.mongodb.net/?retryWrites=true&w=majority`
+    const ruta = await moongose.connect(URL)
+    return ruta
+}
+module.exports = connection */
